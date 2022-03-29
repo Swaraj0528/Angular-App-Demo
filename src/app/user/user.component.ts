93ee2ec7437f991f5b9a19dc37f932908c56a247
Swaraj Varma <swaraj.varma@outlook.com>
2022-03-29 18:29:21 +0530

    One way Data Binding Example
    Done some changes to check one way data binding

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from './User';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent implements OnInit {
+
+  User: User={
+    "id":890,
+    "name": "swaraj varma",
+    "email" :"swaraj.varma@outllook.com",
+    "mobile":7890501502
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
